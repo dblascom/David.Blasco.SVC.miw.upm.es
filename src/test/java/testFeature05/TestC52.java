81614c455386723d00d1dbfc29b3dca5b925d5d3
David Blasco <dblasco@gmail.com>
2016-10-03 22:27:01 +0200

    Create TestC52

---
+++ src/test/java/testFeature05/TestC52.java
@@ -0,0 +1,14 @@
+package testFeature05;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestC52 {
+
+    @Test
+    public void testMA() {
+        fail("Not yet implemented");
+    }
+
+}
