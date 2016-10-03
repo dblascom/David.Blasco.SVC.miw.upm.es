8eb3656a90f74de61697032d554d7b4e7c37bf95
David Blasco <dblasco@gmail.com>
2016-10-03 20:19:51 +0200

    Create TestC22

---
+++ src/test/java/testFeature02/TestC22.java
@@ -0,0 +1,14 @@
+package testFeature02;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestC22 {
+
+    @Test
+    public void testMA() {
+        fail("Not yet implemented");
+    }
+
+}
