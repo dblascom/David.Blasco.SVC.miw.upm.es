e35f974ecd79be5a8a97ac0c5fc4e47aad69e514
David Blasco <dblasco@gmail.com>
2016-10-03 21:18:30 +0200

    Create TestC51

---
+++ src/test/java/testFeature05/TestC51.java
@@ -0,0 +1,19 @@
+package testFeature05;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestC51 {
+
+    @Test
+    public void testM1() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testM2() {
+        fail("Not yet implemented");
+    }
+
+}
